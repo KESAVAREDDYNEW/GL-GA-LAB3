17f76533f1776b63a8c320fc1b5d061bbdddd237
KESAVAREDDYNEW <103446052+KESAVAREDDYNEW@users.noreply.github.com>
2022-05-01 22:53:51 +0400

    Add files via upload

---
+++ labSession3/src/labSession3/BalancingBrackets.java
@@ -0,0 +1,57 @@
+package labSession3;
+
+import java.util.Stack;
+public class BalancingBrackets {
+ 
+	static boolean CheckingBarcketsForBalancing(String bracketExpression)
+	{
+		Stack <Character> stack = new Stack<Character>();
+for (int i=0; i<bracketExpression.length(); i++)
+		{
+			char character = bracketExpression.charAt(i);
+			if (character == '('|| character =='['|| character == '{')
+			{
+				stack.push(character);
+				continue;
+			}
+			if(stack.isEmpty())
+			return false;
+			
+			char c ;
+			switch (character) {
+			
+			case '}':
+				c= stack.pop();
+				if(c=='('|| c=='[')
+					return false;
+				break;
+				
+			case ')':
+				c= stack.pop();
+				if(c=='{'|| c=='[')
+					return false;
+				break;
+				
+			case ']':
+				c= stack.pop();
+				if(c=='('|| c=='{')
+					return false;
+				break;
+			}
+		}
+		return (stack.isEmpty());
+	}
+public static void main (String[] args) {
+	
+	String bracketExpression = "([[{}]])";
+	Boolean result;
+	result = CheckingBarcketsForBalancing(bracketExpression);
+	if(result)
+		System.out.println(" The entered baracket expression  has balanced Brackets");
+	else
+	
+		System.out.println(" The entered baracket expression   NOT  balanced");
+
+}
+
+}
